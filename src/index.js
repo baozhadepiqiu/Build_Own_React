e57e1a3d4be503e11ef95bd2abc29add207330ea
LCG <2390713308@qq.com>
2022-07-23 23:18:01 +0800

    完成STEP4：构建Fiber树和遍历Fiber树

--- src/index.js
+++ src/index.js
@@ -32,34 +32,35 @@ function createTextElement(text) {
   };
 }
 
-/**
- * 渲染DOM函数 将需要渲染的DOM节点挂载到真实的DOM上
- * @param {Element} element 需要渲染的DOM
- * @param {Element} container DOM挂载的真实DOM
- */
-function render(element, container) {
+function createdom(fiber) {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(element.type);
   // 过滤回调 筛选出所有不是children的属性
   const isProperty = key => key !== "children";
   // 遍历当前节点的所有属性 并将其添加
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(name => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
-  // 递归的为每一个子节点渲染冰挂载到当前节点
-  element.props.children.forEach(child => render(child, dom));
-  // 当前节点所有的节点全部完成后一次性挂载到 container 容器中
-  container.appendChild(dom);
+  return dom
+}
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
 }
 
 // 初始化下一个工作单元
 const nextUnitOfWork = null
 // 工作循环
-function workLoop() {
+function workLoop(deadline) {
   let shouldYeild = false
   while (nextUnitOfWork && !shouldYeild) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
@@ -72,9 +73,67 @@ function workLoop() {
  */
 requestIdleCallback(workLoop)
 
-// 此函数会执行第一个 工作单元 并且会返回下一个需要执行的工作单元
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
+/**
+ * 创建并添加一个DOM
+ * 创建一个信的Fiber
+ * 返回下一个工作单元
+ * @param {Fiber} fiber 
+ */
+function performUnitOfWork(fiber) {
+  // 若当前的Fiber 没有 dom 这个属性 则使用 createdom 为其创建这个属性 
+  if (!fiber.dom) {
+    fiber.dom = createdom(fiber)
+  }
+
+  // 若fiber 有 父节点 则在其父节点上添加当前 feiber.dom 用于保持跟踪
+  // We keep track of the DOM node in the fiber.dom property.
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  // 为每一个孩子节点创建 一个新的 Fiber
+  const elements = fiber.props.children
+  let index = 0
+  let presilibing = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    }
+    // endOf 创建新的Fiber
+    
+    // TODO create new fibers
+    // 如果是第一个节点 将使其变为孩子节点
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      // 若不是第一个节点 就将其设置为第一个孩子节点的兄弟节点
+      presilibing.silibing = newFiber
+    }
+
+    presilibing.silibing = newFiber
+    index++
+  }
+
+  // TODO return next unit of work
+  // 保持对Fiber的跟踪 以此来寻找下一个工作单元 返回下一个渲染的工作单元
+  // 若当前的Fiber有子节点 则下一个工作单元为其子节点
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  // 如果没有子节点 但是有其兄弟节点  则返回其兄弟节点
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.silibing) {
+      return nextFiber.silibing
+    }
+    nextFiber = nextFiber.silibing
+  }
 }
 
 const Didact = {
